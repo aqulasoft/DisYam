979ce2773e479fa0728a1e6d12a107792037b14f
Forichok <nnudalov@gmail.com>
2021-01-11 01:39:40 +0300

    Add check for album parsing

--- src/main/java/com/aqulasoft/disyam/models/audio/YaAlbum.java
+++ src/main/java/com/aqulasoft/disyam/models/audio/YaAlbum.java
@@ -11,9 +11,9 @@ import java.util.List;
 public class YaAlbum {
     private final long id;
     private int year;
-    private final String genre;
+    private String genre;
     private final String title;
-    private final int trackCount;
+    private int trackCount;
     //    private Date releaseDate;
     private final List<String> labels;
     private final List<YaArtist> artists;
@@ -21,25 +21,29 @@ public class YaAlbum {
     public YaAlbum(JSONObject json) {
         id = json.getLong("id");
         if (json.has("year")) year = json.getInt("year");
-        genre = json.getString("genre");
+        if (json.has("genre")) genre = json.getString("genre");
         title = json.getString("title");
-        trackCount = json.getInt("trackCount");
+        if (json.has("trackCount")) trackCount = json.getInt("trackCount");
         artists = new ArrayList<>();
-        JSONArray jsonArtistArray = json.getJSONArray("artists");
-        for (int i = 0; i < jsonArtistArray.length(); i++) {
-            JSONObject artist = jsonArtistArray.getJSONObject(i);
-            artists.add(new YaArtist(artist));
+        if (json.has("artists")) {
+            JSONArray jsonArtistArray = json.getJSONArray("artists");
+            for (int i = 0; i < jsonArtistArray.length(); i++) {
+                JSONObject artist = jsonArtistArray.getJSONObject(i);
+                artists.add(new YaArtist(artist));
+            }
         }
 
         labels = new ArrayList<>();
-        for (Object label :
-                json.getJSONArray("labels")
-        ) {
-            if (label instanceof String)
-                labels.add((String) label);
-            else
-                labels.add(((JSONObject) label).getString("name"));
+        if (json.has("labels")) {
+            for (Object label :
+                    json.getJSONArray("labels")
+            ) {
+                if (label instanceof String)
+                    labels.add((String) label);
+                else
+                    labels.add(((JSONObject) label).getString("name"));
 
+            }
         }
 
     }
